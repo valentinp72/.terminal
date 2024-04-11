3a21044a16b54b5bfc4d771a4fc4ec87d1051d05
Valentin Pelloin <valentinpelloin@gmail.com>
2024-04-11 09:50:51 +0200

    configured multi screen workspaces

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,4 +2,12 @@
 
 polybar-msg cmd quit
 
-polybar base &
+# polybar base &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload base &
+  done
+else
+  polybar --reload base &
+fi
