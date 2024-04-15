2b4b694defeac8c327b7aa0de7228912e8a38d02
Valentin Pelloin <valentinpelloin@gmail.com>
2024-04-15 19:12:40 +0200

    polybar with network status and cpu usage ; keybinds

---
+++ .config/rofi/applets/bin/files.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+theme="$HOME/.config/rofi/launchers/type-7/style-7.rasi"
+
+rofi -show filebrowser -modi filebrowser -show-icons -no-show-match -no-sort -theme $theme -theme-str "window {width: 800px;}"
+
+
